f3a57d59a99fd5af1f5d1e3231d69282f716f22d
damccull <401104+damccull@users.noreply.github.com>
2024-06-16 23:27:43 -1000

    Add autocommand for highlight yanked text

--- nvim/plugin/autocommands.lua
+++ nvim/plugin/autocommands.lua
@@ -127,6 +127,18 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end,
 })
 
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+
 -- More examples, disabled by default
 
 -- Toggle between relative/absolute line numbers
