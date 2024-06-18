6e302773cc8534be634b8d37f7466a7b176be3d9
damccull <401104+damccull@users.noreply.github.com>
2024-06-17 21:59:08 -1000

    Use nixpkgs-fmt to format nix code

--- nvim/ftplugin/nix.lua
+++ nvim/ftplugin/nix.lua
@@ -15,4 +15,11 @@ vim.lsp.start {
   cmd = { 'nil' },
   root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
   capabilities = require('user.lsp').make_client_capabilities(),
+  settings = {
+    ['nil'] = {
+      formatting = {
+        command = { "nixpkgs-fmt" },
+      },
+    },
+  },
 }
