ba86705053fe294c467baca97528b6fbcbb270a1
damccull <401104+damccull@users.noreply.github.com>
2024-08-14 21:41:09 -1000

    Revert "Remove eyeliner for now due to it being broken"
    This reverts commit 7d3f686ea9da7344f848884b20244b1195d8a1f1.

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -37,7 +37,7 @@ with final.pkgs.lib; let
     nvim-autopairs
     comment-nvim # Allows commenting visual regions or lines
     conform-nvim # Autoformatting
-    # eyeliner-nvim # Highlights unique characters for f/F and t/T motions | https://github.com/jinh0/eyeliner.nvim
+    eyeliner-nvim # Highlights unique characters for f/F and t/T motions | https://github.com/jinh0/eyeliner.nvim
     fidget-nvim # UI for Neovim notifications and LSP status/progress
     gitsigns-nvim # Adds git related signs to the gutter and utilities for managing changes
     indent-blankline-nvim
