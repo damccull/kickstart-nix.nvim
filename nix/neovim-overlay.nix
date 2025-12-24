522cd1349065e1e5770c9592fbb3db26ad812c4b
damccull <401104+damccull@users.noreply.github.com>
2025-12-23 22:56:37 -1000

    Update 'system' to newer location

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -23,7 +23,7 @@ let
 
   # Make sure we use the pinned nixpkgs instance for wrapNeovimUnstable,
   # otherwise it could have an incompatible signature when applying this overlay.
-  pkgs-wrapNeovim = inputs.nixpkgs.legacyPackages.${pkgs.system};
+  pkgs-wrapNeovim = inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
 
   # This is the helper function that builds the Neovim derivation.
   mkNeovim = pkgs.callPackage ./mkNeovim.nix { inherit pkgs-wrapNeovim; };
