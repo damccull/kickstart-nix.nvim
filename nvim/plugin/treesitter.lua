31799e5445acea4eb160f5431f414b8945031054
damccull <401104+damccull@users.noreply.github.com>
2026-02-03 08:58:55 -0600

    Re-enable treesitter-context config

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -186,12 +186,12 @@ vim.g.did_load_treesitter_plugin = true
 --   },
 -- }
 --
--- require('treesitter-context').setup {
---   max_lines = 3,
--- }
---
--- require('ts_context_commentstring').setup()
---
--- -- Tree-sitter based folding
--- -- vim.opt.foldmethod = 'expr'
--- vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+require('treesitter-context').setup {
+  max_lines = 3,
+}
+
+require('ts_context_commentstring').setup()
+
+-- Tree-sitter based folding
+-- vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
